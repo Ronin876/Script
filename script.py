59a1e5033fddfefd7672d2ab645c5f5d7c131b90
Пешков Роман Игоревич <peshkovri@aup.vogu>
2023-07-11 12:39:50 +0300

    1st commit

---
+++ script.py
@@ -0,0 +1,18 @@
+import shutil
+import os
+
+
+def deleteDirectory(array):
+    if os.name == "nt":
+        path = "D:\\tree\\"
+    else:
+        path = "D:/tree/"
+    for i in array:
+        try:
+            name = i
+            i = path + i
+            shutil.rmtree(i)
+        except OSError as error:
+            print("Error - ", name)
+            continue
+
